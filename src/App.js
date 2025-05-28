2e3cafd2b73c4efa8a4cc0745cc136625c2c1408
James Bradley <jab.bradley99@gmail.com>
2025-05-28 16:33:08 +0100

    restore functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState('');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:9000/searchAlbums/${searchText}`)
+      .then(res => res.json())
+      .then(data => setResults(JSON.stringify(data)))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <p>{results}</p>
     </div>
   );
 }
