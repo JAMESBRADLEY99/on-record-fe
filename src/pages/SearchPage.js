8c1ba567d5d25b4a9185ec560c62bc46afac7c2b
James Bradley <jab.bradley99@gmail.com>
2025-07-17 13:57:49 +0100

    add routing

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function SearchPage() {
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!searchText) {
+      return;
+    }
+    fetch(`http://localhost:9000/searchAlbums/${searchText}`)
+      .then(res => res.json())
+      .then(data => setResults(data))
+  };
+
+  return (
+    <div className="main">
+      <h1>Search</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ul className="no-bullets">
+        {results.map((item, index) => (
+          <>
+          <li key={index}>
+            <a href={`https://open.spotify.com/album/${item.album_id}`}>{item.name}</a>
+          </li>
+            <p className="artist" >{item.artist}</p>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchPage;
